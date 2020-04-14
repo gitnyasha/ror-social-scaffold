057380c52697c76d0b53428fbb23600646e2338b
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-04-14 21:08:37 +0200

    created a friendship button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,11 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     @users = User.all
+    @friends = current_user.friends
   end
 
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
-  end
-
-  def friend_invite
-    @other_user = User.find(params[:id])
-    flash[:success] = "This user will get a friend request."
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_04_13_201826) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
@@ -26,13 +23,13 @@ ActiveRecord::Schema.define(version: 2020_04_13_201826) do
   end
 
   create_table "friendships", force: :cascade do |t|
-    t.bigint "users_id"
-    t.bigint "friend_id"
+    t.integer "user_id"
+    t.integer "friend_id"
     t.boolean "confirmed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
-    t.index ["users_id"], name: "index_friendships_on_users_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -64,6 +61,4 @@ ActiveRecord::Schema.define(version: 2020_04_13_201826) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "friendships", "users", column: "friend_id"
-  add_foreign_key "friendships", "users", column: "users_id"
 end
