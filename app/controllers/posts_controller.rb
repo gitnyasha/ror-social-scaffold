68349d704b17d6081c439bc0ab58f3c73a920bda
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-04-20 17:32:30 +0200

    rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,22 +10,22 @@ class PostsController < ApplicationController
     @post = current_user.posts.new(post_params)
 
     if @post.save
-      redirect_to posts_path, notice: 'Post was successfully created.'
+      redirect_to posts_path, notice: "Post was successfully created."
     else
       timeline_posts
-      render :index, alert: 'Post was not created.'
+      render :index, alert: "Post was not created."
     end
   end
 
   private
 
   def timeline_posts
-  friends_posts = current_user.posts
-  current_user.friends.each do |friend|
-    friends_posts += friend.posts
+    friends_posts = current_user.posts
+    current_user.friends.each do |friend|
+      friends_posts += friend.posts
+    end
+    @timeline_posts = friends_posts.sort { |a, b| b.updated_at <=> a.updated_at }
   end
-  @timeline_posts = friends_posts.sort { |a, b| b.updated_at <=> a.updated_at }
-end
 
   def post_params
     params.require(:post).permit(:content)
